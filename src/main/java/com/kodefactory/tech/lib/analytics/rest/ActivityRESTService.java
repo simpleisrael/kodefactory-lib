d37018d093b73be7b576e0ea7f776ef7ba5eecc4
simpleisrael <rvec007@yahoo.com>
2018-05-17 22:35:05 -0400

    ActivityRESTService Updated

--- src/main/java/com/kodefactory/tech/lib/analytics/rest/ActivityRESTService.java
+++ src/main/java/com/kodefactory/tech/lib/analytics/rest/ActivityRESTService.java
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("analytics-service")
 public class ActivityRESTService extends BaseREST {
     private ActivityService activityService;
 
